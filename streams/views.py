741c95267ad8f4d5f34ee327e1df72b627ecad2e
viraj <virajshivaji.shedge@mail.ivtree.com>
2025-05-18 12:05:55 +0530

    testing

--- streams/views.py
+++ streams/views.py
@@ -1,5 +1,10 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 def index(request):
     return HttpResponse("RTSP Stream Viewer Home")
+
+
+
+def ping_view(request):
+    return JsonResponse({'message': 'pong from Django!'})
